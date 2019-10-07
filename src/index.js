ab3ee221d716f1e10019174bfb17456d9ff8474a
Jack McCown <jackmac92@protonmail.com>
2019-10-06 20:20:59 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-export function run() {
-  console.log('Fuck off world!');
+const fs = require('fs')
+const path = require('path')
+const { name: pkgName, version, description } = require('./package.json')
+
+const isCodeFile = file =>
+  ['.js', '.ts', '.jsx', '.tsx'].some(ext => file.endsWith(ext))
+
+const listFiles = folder =>
+  fs
+    .readdirSync(folder)
+    .filter(
+      file => isCodeFile(file) && fs.lstatSync(path.join(folder, file)).isFile()
+    )
+
+const getUrlMatches = scriptPath => {
+  const scriptModuleLines = fs
+    .readFileSync(`./${scriptPath}`, 'utf8')
+    .split('\n')
+  if (scriptModuleLines[0].includes('matches')) {
+    let isCollectionMatchesLines = true
+    return scriptModuleLines.slice(1).reduce((acc, el) => {
+      if (!el.startsWith('//')) {
+        isCollectionMatchesLines = false
+      }
+      if (!isCollectionMatchesLines) {
+        return acc
+      }
+      return [...acc, el.replace('//', '').trim()]
+    }, [])
+  } else {
+    console.warn('No match urls listed, so matching against all urls')
+    return ['*://*/*']
+  }
+}
+
+const autoGenContentScripts = contentScriptsDir =>
+  listFiles(contentScriptsDir).map(s => {
+    const scriptPath = path.join(contentScriptsDir, s)
+    const matches = getUrlMatches(scriptPath)
+    return { matches, js: `./${scriptPath}` }
+  })
+
+const injectScriptsDir = './src/injectedScripts'
+
+console.log(`Looking in ${injectScriptsDir} for web-ext content scripts`)
+
+module.exports.run = () => {
+  fs.writeFileSync(
+    './manifest.json',
+    JSON.stringify(
+      {
+        name: pkgName,
+        version,
+        description,
+        manifest_version: 2,
+        permissions: process.env.WEB_EXT_PERMISSIONS,
+        content_scripts: autoGenContentScripts()
+      },
+      null,
+      4
+    )
+  )
+
+  console.log('Wrote Manifest Successfully')
 }
