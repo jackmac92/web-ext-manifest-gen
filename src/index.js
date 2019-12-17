e5580a495f0ecc3c68c100e0eaf428e490f1b470
Jack McCown <jmccown@cbinsights.com>
2019-12-17 16:45:13 -0500

    fix: found the real problem

--- src/index.js
+++ src/index.js
@@ -41,123 +41,111 @@ const autoGenContentScripts = contentScriptsDir =>
     return { matches, js: [`./${scriptPath}`] }
   })
 
-module.exports.run = () =>
-  new Promise((resolve, reject) => async () => {
-    if (!fs.existsSync(`${appRootPath}/package.json`)) {
-      console.error('Please run from an npm project')
-      console.error('No package.json found!!')
-      process.exit(1)
-    }
-    const {
-      name: pkgName,
-      version,
-      description
-    } = require(`${appRootPath}/package.json`)
-    // TODO copy autogen permissions setup from that rollup lib
-    // TODO handle content_security_policy
-    // TODO handle icons
-    const argv = require('yargs')
-      .usage('Usage: $0 -s [injectScriptsDir] -p [permission]')
-      .option('scripts', {
-        alias: 's',
-        describe: 'path to content scripts'
-      })
-      .option('devTools', {
-        describe: 'path to dev-tools html'
-      })
-      .option('template', {
-        alias: 't',
-        describe:
-          'path to an existing manifest file which should be used for supplementary keys (will be overriden)'
-      })
-      .option('backgroundScripts', {
-        type: 'array',
-        describe: 'path to background file, multiple entries allowed'
-      })
-      .option('locale', {
-        describe: 'path to background file, multiple entries allowed',
-        default: 'en'
-      })
-      .option('permissions', {
-        alias: 'p',
-        type: 'array',
-        describe: 'permissions to include in manifest',
-        default: ['activeTab']
-      })
-      .option('optionalPermissions', {
-        type: 'array',
-        describe: 'optional-permissions to include in manifest'
-      })
-      .demandOption(['scripts']).argv
-    const injectScriptsDir = argv.scripts
-    const svgResponse = await fetch(
-      `https://svgen-logo.jackmac92.now.sh/api/create?text=${encodeURIComponent(
-        pkgName
-      )}&size=300`
-    )
-    const svgContents = await svgResponse.arrayBuffer()
-    const generatedIconPath = './stock-generated-icon.svg'
-    fs.writeFileSync(generatedIconPath, Buffer.from(svgContents))
+module.exports.run = async () => {
+  if (!fs.existsSync(`${appRootPath}/package.json`)) {
+    console.error('Please run from an npm project')
+    console.error('No package.json found!!')
+    process.exit(1)
+  }
+  const {
+    name: pkgName,
+    version,
+    description
+  } = require(`${appRootPath}/package.json`)
+  // TODO copy autogen permissions setup from that rollup lib
+  // TODO handle content_security_policy
+  // TODO handle icons
+  const argv = require('yargs')
+    .usage('Usage: $0 -s [injectScriptsDir] -p [permission]')
+    .option('scripts', {
+      alias: 's',
+      describe: 'path to content scripts'
+    })
+    .option('devTools', {
+      describe: 'path to dev-tools html'
+    })
+    .option('template', {
+      alias: 't',
+      describe:
+        'path to an existing manifest file which should be used for supplementary keys (will be overriden)'
+    })
+    .option('backgroundScripts', {
+      type: 'array',
+      describe: 'path to background file, multiple entries allowed'
+    })
+    .option('locale', {
+      describe: 'path to background file, multiple entries allowed',
+      default: 'en'
+    })
+    .option('permissions', {
+      alias: 'p',
+      type: 'array',
+      describe: 'permissions to include in manifest',
+      default: ['activeTab']
+    })
+    .option('optionalPermissions', {
+      type: 'array',
+      describe: 'optional-permissions to include in manifest'
+    })
+    .demandOption(['scripts']).argv
+  const injectScriptsDir = argv.scripts
+  const svgResponse = await fetch(
+    `https://svgen-logo.jackmac92.now.sh/api/create?text=${encodeURIComponent(
+      pkgName
+    )}&size=300`
+  )
+  const svgContents = await svgResponse.arrayBuffer()
+  const generatedIconPath = './stock-generated-icon.svg'
+  fs.writeFileSync(generatedIconPath, Buffer.from(svgContents))
 
-    // COULDDO optimize svg https://github.com/svg/svgo
-    const easilyOverridableDefaults = {
-      permissions: [],
-      optional_permissions: [],
-      version,
-      description,
-      browser_action: {
-        default_icon: { 32: generatedIconPath },
-        default_title: pkgName
-      },
-      icons: { 128: generatedIconPath }
+  // COULDDO optimize svg https://github.com/svg/svgo
+  const easilyOverridableDefaults = {
+    permissions: [],
+    optional_permissions: [],
+    version,
+    description,
+    browser_action: {
+      default_icon: { 32: `${generatedIconPath}-testing` },
+      default_title: pkgName
+    },
+    icons: { 128: `${generatedIconPath}` }
+  }
+  const manifestBase = (() => {
+    try {
+      const fileContents = fs.readFileSync(argv.template)
+      return JSON.parse(fileContents)
+    } catch (e) {
+      console.warn('Failed to parse a tempalte manifest, using empty object!')
+      return {}
     }
-    const manifestBase = (() => {
-      try {
-        const fileContents = fs.readFileSync(argv.template)
-        return JSON.parse(fileContents)
-      } catch (e) {
-        console.warn('Failed to parse a tempalte manifest, using empty object!')
-        return {}
-      }
-    })()
-    const manifest = Object.assign(
-      {},
-      easilyOverridableDefaults,
-      manifestBase,
-      {
-        manifest_version: 2,
-        default_locale: argv.locale,
-        name: pkgName,
-        content_scripts: autoGenContentScripts(injectScriptsDir)
-      }
-    )
-    if (argv.devTools) {
-      manifest.devtools_page = argv.devTools
+  })()
+  const manifest = Object.assign({}, easilyOverridableDefaults, manifestBase, {
+    manifest_version: 2,
+    default_locale: argv.locale,
+    name: pkgName,
+    content_scripts: autoGenContentScripts(injectScriptsDir)
+  })
+  if (argv.devTools) {
+    manifest.devtools_page = argv.devTools
+  }
+  if (argv.backgroundScripts) {
+    manifest.background = {
+      scripts: argv.backgroundScripts,
+      persistent: true
     }
-    if (argv.backgroundScripts) {
-      manifest.background = {
-        scripts: argv.backgroundScripts,
-        persistent: true
-      }
-    }
-    if (argv.optionalPermissions) {
-      argv.optionalPermissions.forEach(perm => {
-        if (!manifest['optional_permissions'].includes(perm)) {
-          manifest['optional_permissions'].push(perm)
-        }
-      })
-    }
-
-    argv.permissions.forEach(perm => {
-      if (!manifest.permissions.includes(perm)) {
-        manifest.permissions.push(perm)
+  }
+  if (argv.optionalPermissions) {
+    argv.optionalPermissions.forEach(perm => {
+      if (!manifest['optional_permissions'].includes(perm)) {
+        manifest['optional_permissions'].push(perm)
       }
     })
+  }
 
-    try {
-      fs.writeFileSync('./manifest.json', JSON.stringify(manifest, null, 4))
-    } catch (e) {
-      reject(e)
+  argv.permissions.forEach(perm => {
+    if (!manifest.permissions.includes(perm)) {
+      manifest.permissions.push(perm)
     }
-    resolve()
   })
+  fs.writeFileSync('./manifest.json', JSON.stringify(manifest, null, 4))
+}
