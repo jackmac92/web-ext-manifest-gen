65799050b29e3d6675119e038babd700cefd8f85
Jack McCown <jmccown@cbinsights.com>
2019-12-18 23:31:23 -0500

    Revert "feat: initial working setup"
    This reverts commit aa50b60fb12fdecaae05337281924f03acc88020.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,30 @@
-const write = require('write')
-const sharp = require('sharp')
-const fetch = require('node-fetch')
-
-const svgGen = async (text, size, otherOptions = {}) => {
-  const params = new URLSearchParams()
-  const opts = Object.assign({}, otherOptions, { text, size })
-  for (const p in opts) {
-    params.append(p, opts[p])
-  }
-  const svgResponse = await fetch(
-    `https://svgen-logo.jackmac92.now.sh/api/create?${params}`
-  )
-  const svgArrayBuffer = await svgResponse.arrayBuffer()
-  return Buffer.from(svgArrayBuffer)
+function main(argz) {
+  return Promise.resolve().then(() => {
+    console.log('Hello world!')
+    console.dir(argz)
+  })
 }
 
-const svg2png = async (svg, sizes = [32]) => {
-  for (const sz of sizes) {
-    const png = await sharp(svg)
-      .resize(sz, sz)
-      .png()
-      .toBuffer()
-    await write(`./icons/icon-${sz}.png`, png)
-  }
+module.exports.main = main
+
+function getArgs(argv) {
+  const args = require('yargs/yargs')(argv.slice(2))
+    .command('execute [action]', 'execute specified action', yargs => {
+      yargs.positional('action', {
+        describe: 'cli action',
+        default: 'unknown'
+      })
+    })
+    .command('modify [target]', 'invoke cli against target', yargs => {
+      yargs.positional('target', {
+        describe: 'cli target',
+        default: 'unknown'
+      })
+    }).argv
+
+  return Promise.resolve(args)
 }
 
-module.exports.run = async () => {
-  const argv = require('yargs')
-    .option('toPng', {
-      type: 'array',
-      description: 'A list of sizes to generate png logos'
-    })
-    .option('size', {
-      describe: 'Dimension size for the logo',
-      type: 'number',
-      default: 200
-    })
-    .option('text', {
-      describe: 'Text for the logo'
-    })
-    .demandOption(['text']).argv
+module.exports.getArgs = getArgs
 
-  const svgContents = await svgGen(argv.text, argv.size)
-
-  if (argv.toPng) {
-    await svg2png(svgContents, argv.toPng)
-  } else {
-    await write('./icon.svg', svgContents)
-  }
-}
+module.exports.run = (...args) => getArgs(args).then(main)
