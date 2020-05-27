c506c7d624300e1fe236fb6e9601c5ef6a9ebe60
Jack McCown <jmccown@cbinsights.com>
2020-05-26 21:43:52 -0400

    chore: convert to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,12 @@
-const fs = require("fs");
-const glob = require("glob");
-const write = require("write");
-const path = require("path");
-const dependencyTree = require("dependency-tree");
-const appRootPath = require("app-root-path");
-const pkgDir = require("pkg-dir");
-
-const genRegex = perm =>
-  new RegExp(`(chromep?|browser)[\\s\\n]*\\.[\\s\\n]*${perm}`);
-
-const ALL_PERMISSIONS = {
-  alarms: s => genRegex("alarms").test(s),
-  bookmarks: s => genRegex("bookmarks").test(s),
-  contentSettings: s => genRegex("contentSettings").test(s),
-  contextMenus: s => genRegex("contextMenus").test(s),
-  cookies: s => genRegex("cookies").test(s),
-  declarativeContent: s => genRegex("declarativeContent").test(s),
-  declarativeNetRequest: s => genRegex("declarativeNetRequest").test(s),
-  declarativeWebRequest: s => genRegex("declarativeWebRequest").test(s),
-  desktopCapture: s => genRegex("desktopCapture").test(s),
-  displaySource: s => genRegex("displaySource").test(s),
-  dns: s => genRegex("dns").test(s),
-  documentScan: s => genRegex("documentScan").test(s),
-  downloads: s => genRegex("downloads").test(s),
-  experimental: s => genRegex("experimental").test(s),
-  fileBrowserHandler: s => genRegex("fileBrowserHandler").test(s),
-  fileSystemProvider: s => genRegex("fileSystemProvider").test(s),
-  fontSettings: s => genRegex("fontSettings").test(s),
-  gcm: s => genRegex("gcm").test(s),
-  geolocation: s => genRegex("geolocation").test(s),
-  history: s => genRegex("history").test(s),
-  identity: s => genRegex("identity").test(s),
-  idle: s => genRegex("idle").test(s),
-  idltest: s => genRegex("idltest").test(s),
-  management: s => genRegex("management").test(s),
-  nativeMessaging: s => genRegex("nativeMessaging").test(s),
-  notifications: s => genRegex("notifications").test(s),
-  pageCapture: s => genRegex("pageCapture").test(s),
-  platformKeys: s => genRegex("platformKeys").test(s),
-  power: s => genRegex("power").test(s),
-  printerProvider: s => genRegex("printerProvider").test(s),
-  privacy: s => genRegex("privacy").test(s),
-  processes: s => genRegex("processes").test(s),
-  proxy: s => genRegex("proxy").test(s),
-  sessions: s => genRegex("sessions").test(s),
-  signedInDevices: s => genRegex("signedInDevices").test(s),
-  storage: s => genRegex("storage").test(s),
-  tabCapture: s => genRegex("tabCapture").test(s),
-  // tabs: s => /(chromep?|browser)[\s\n]*\.[\s\n]*tabs/.test(s),
-  topSites: s => genRegex("topSites").test(s),
-  tts: s => genRegex("tts").test(s),
-  ttsEngine: s => genRegex("ttsEngine").test(s),
-  unlimitedStorage: s => genRegex("unlimitedStorage").test(s),
-  vpnProvider: s => genRegex("vpnProvider").test(s),
-  wallpaper: s => genRegex("wallpaper").test(s),
-  webNavigation: s => genRegex("webNavigation").test(s),
-  webRequest: s => genRegex("webRequest").test(s),
-  webRequestBlocking: s =>
-    ALL_PERMISSIONS.webRequest(s) && s.includes("'blocking'")
-};
+import fs from "fs";
+import glob from "glob";
+import write from "write";
+import path from "path";
+import dependencyTree from "dependency-tree";
+import appRootPath from "app-root-path";
+import pkgDir from "pkg-dir";
+import identifyRequiredPerms from "./permissionExtractor";
+import { JSONSchemaForGoogleChromeExtensionManifestFiles as ExtensionManifest } from "./browser-extension-manifest";
 
 const findAllDependentFiles = () =>
   new Promise((resolve, reject) => {
@@ -70,7 +18,7 @@ const findAllDependentFiles = () =>
       resolve(files.filter(f => f.indexOf("node_modules") === -1));
     });
   })
-    .then(async files => {
+    .then(async (files: string[]) => {
       const directory = await pkgDir(__dirname);
       // let tsConfig;
       // const tsConfigPath = `${directory}/tsconfig.json`;
@@ -78,6 +26,7 @@ const findAllDependentFiles = () =>
       //   tsConfig = tsConfigPath;
       // }
       return files.reduce((acc, f) => {
+        // @ts-expect-error
         global.window = global.window || true;
         dependencyTree
           .toList({
@@ -100,7 +49,7 @@ const findAllDependentFiles = () =>
 const findPermissions = () =>
   findAllDependentFiles().then(files =>
     Promise.all(
-      files.map(f =>
+      files.map((f: string) =>
         new Promise(resolve => {
           fs.readFile(f, {}, (err, data) => {
             if (err) {
@@ -114,10 +63,9 @@ const findPermissions = () =>
     )
       .then(filesWithContents =>
         filesWithContents.reduce((acc, [fileName, fileContents]) => {
-          const foundPermissions = Object.entries(ALL_PERMISSIONS)
-            .filter(([_, permTest]) => permTest(fileContents))
-            .map(([permType]) => permType);
-
+          const foundPermissions = identifyRequiredPerms(
+            fileContents as Buffer
+          );
           for (const permType of foundPermissions) {
             console.log(
               `${fileName} needs ${permType} in the permission set, adding.`
@@ -150,37 +98,51 @@ const listFiles = folder =>
     )
   );
 
-const getUrlMatches = scriptPath => {
+const getUrlMatches = (scriptPath: string): [string, ...string[]] => {
   const scriptModuleLines = fs
     .readFileSync(`./${scriptPath}`, "utf8")
     .split("\n");
   if (scriptModuleLines[0].includes("matches")) {
     let isCollectionMatchesLines = true;
-    return scriptModuleLines.slice(1).reduce((acc, el) => {
-      if (!el.startsWith("//")) {
-        isCollectionMatchesLines = false;
-      }
-      if (!isCollectionMatchesLines) {
-        return acc;
-      }
-      return [...acc, el.replace("//", "").trim()];
-    }, []);
+    return scriptModuleLines.slice(1).reduce(
+      (acc, el) => {
+        if (!el.startsWith("//")) {
+          isCollectionMatchesLines = false;
+        }
+        if (!isCollectionMatchesLines) {
+          return acc;
+        }
+        return [...acc, el.replace("//", "").trim()];
+      },
+      [""]
+    );
   } else {
     console.warn("No match urls listed, so matching against all urls");
     return ["*://*/*"];
   }
 };
 
-const autoGenContentScripts = contentScriptsDir =>
-  listFiles(contentScriptsDir).then(o =>
-    o.map(s => {
-      const scriptPath = path.join(contentScriptsDir, s);
-      const matches = getUrlMatches(scriptPath);
-      return { matches, js: [`./${scriptPath}`] };
-    })
-  );
+type ContentScripts = ExtensionManifest["content_scripts"];
 
-module.exports.run = async () => {
+const createContentScript = (contentScriptsDir: string) => (
+  s: string
+): ContentScripts[0] => {
+  const scriptPath = path.join(contentScriptsDir, s);
+  const matches = getUrlMatches(scriptPath);
+  return { matches, js: [`./${scriptPath}`] };
+};
+
+const autoGenContentScripts = (
+  contentScriptsDir: string
+): Promise<ContentScripts | null> =>
+  listFiles(contentScriptsDir).then(o => {
+    if (o.length < 1) {
+      return null;
+    }
+    return o.map(createContentScript(contentScriptsDir)) as ContentScripts;
+  });
+
+export const run = async () => {
   if (!fs.existsSync(`${appRootPath}/package.json`)) {
     console.error("Please run from an npm project");
     console.error("No package.json found!!");
@@ -256,22 +218,30 @@ module.exports.run = async () => {
   const manifestBase = (() => {
     try {
       const fileContents = fs.readFileSync(argv.template);
-      return JSON.parse(fileContents);
+      return JSON.parse(fileContents.toString());
     } catch (e) {
       console.warn("Failed to parse a tempalte manifest, using empty object!");
       return {};
     }
   })();
-  const manifest = Object.assign({}, easilyOverridableDefaults, manifestBase, {
-    manifest_version: 2,
-    default_locale: argv.locale,
-    name: pkgName
-  });
+  const manifest: ExtensionManifest = Object.assign(
+    {},
+    easilyOverridableDefaults,
+    manifestBase,
+    {
+      manifest_version: 2,
+      default_locale: argv.locale,
+      name: pkgName
+    }
+  );
   if (argv.scripts) {
-    manifest.content_scripts = autoGenContentScripts(argv.scripts);
+    const generatedContentScripts = await autoGenContentScripts(argv.scripts);
+    if (generatedContentScripts !== null) {
+      manifest.content_scripts = generatedContentScripts;
+    }
   }
-  manifest.permissions = new Set(manifest.permissions);
-  manifest.optional_permissions = new Set(manifest.optional_permissions);
+  manifest.permissions = [];
+  manifest.optional_permissions = [];
 
   if (argv.devTools) {
     manifest.devtools_page = argv.devTools;
@@ -284,14 +254,16 @@ module.exports.run = async () => {
   }
 
   argv.optionalPermissions.forEach(perm => {
-    manifest["optional_permissions"].add(perm);
+    manifest["optional_permissions"].push(perm);
   });
 
   argv.permissions.forEach(perm => {
-    manifest.permissions.add(perm);
+    manifest.permissions.push(perm);
   });
 
-  manifest.permissions = Array.from(manifest.permissions);
-  manifest.optional_permissions = Array.from(manifest.optional_permissions);
+  manifest.permissions = Array.from(new Set(manifest.permissions));
+  manifest.optional_permissions = Array.from(
+    new Set(manifest.optional_permissions)
+  );
   await write("./manifest.json", JSON.stringify(manifest, null, 4));
 };
